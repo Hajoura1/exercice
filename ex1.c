65684f3d93d988f32be9c57df74b0c0297598228
Hajoura1 <elmoutaoukilhajar3@gmail.com>
2024-09-04 09:04:34 +0100

    hajar

--- ex1.c
+++ ex1.c
@@ -1,1 +1,15 @@
-//code
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+   int N ; 
+   printf(" veuiller entrer la valeur de N : ");
+  scanf("%d" ,&N ) ;
+  if (N%2 ==0 ) 
+  
+  printf(" Le nombre %d est un nombre pair" , N);
+  else
+    printf("Le nombre %d est un nombre impair" , N);
+
+    return 0;
+}
